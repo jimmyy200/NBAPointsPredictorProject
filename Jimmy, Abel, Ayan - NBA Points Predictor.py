045e2aec4ef141324a4472b05e1c491973034633
jimmyy200 <jimmy322@my.yorku.ca>
2026-02-19 11:05:36 -0500

    better csv files

--- Jimmy, Abel, Ayan - NBA Points Predictor.py
+++ Jimmy, Abel, Ayan - NBA Points Predictor.py
@@ -5,3 +5,9 @@ from sklearn import datasets
 from sklearn.preprocessing import StandardScaler
 import pandas as pd
 
+file_path = 'C:/Users/jimmy_p2fa2zj/Documents/GitHub/NBAPointsPredictorProject/player.csv'
+
+df = pd.read_csv(file_path)
+
+print(df.head())
+df.info()
